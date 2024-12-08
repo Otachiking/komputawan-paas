f1e252a497cadfa6c95fc1c192df30d41db81e25
Muhammad Iqbal Rasyid (Bal Gans Ea) <otachiking123@gmail.com>
2024-12-08 16:23:17 +0700

    Fix Error

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        radius = float(request.form['radius'])
-        circumference = 2 * 3.14159 * radius
-        area = 3.14159 * radius * radius
-        return render_template('index.html', result=True, radius=radius, 
-                               circumference=circumference, area=area)
-    return render_template('index.html', result=False)
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
